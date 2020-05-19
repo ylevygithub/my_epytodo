30938c0807f17497b12340bd6fe24e613046f45d
yaacov <yaacov.levy@epitech.eu>
2020-05-19 17:52:45 +0200

    epytodo bootstrap

---
+++ bootstrap_epytodo/app/controller.py
@@ -0,0 +1,8 @@
+import api from api
+
+def    route_all_users():
+    controller = MyController()                                                                                                                                             
+    controller.home();                                                                                                                                                      
+    to controller                                                                                                                                                        
+    result = ""
+    try:
